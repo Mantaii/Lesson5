63788b378689efb392ab09a30846f73d4b3a7f24
scottishcoder <owaingdwilliams@gmail.com>
2017-06-26 17:16:10 +0100

    Lesson 5 program

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -10,6 +10,15 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name:");
+            string myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name:");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Helo, " + myFirstName + " " + myLastName);
         }
     }
 }
