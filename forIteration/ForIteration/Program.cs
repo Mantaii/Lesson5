117f3dbe6ea21b44573f9baf9d63fe41a833de9d
scottishcoder <owaingdwilliams@gmail.com>
2017-06-28 13:22:00 +0100

    Finished lesson. For tab tab snippet.

--- forIteration/ForIteration/Program.cs
+++ forIteration/ForIteration/Program.cs
@@ -12,9 +12,21 @@ namespace ForIteration
         {
             for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+                
 
             }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
             Console.ReadLine();
         }
     }
